4b9610f47551d66d7e5e28d435631c904f49fccc
saiprasadrathod1 <saiprasad.rathod1@symbtechnologies.com>
2025-06-22 11:39:32 +0530

    fix: checks added.

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -8,6 +8,16 @@ const router = express.Router();
 
 router.post("/register", async (req, res) => {
   try {
+    // check all fields avaialble or not
+    if (
+      !req.body.firstName ||
+      !req.body.lastName ||
+      !req.body.email ||
+      !req.body.password
+    )
+      throw new Error("firstName,lastName,email,password are required fields");
+
+    // Destructure password
     const { password, ...rest } = req.body;
 
     // Hash the password
@@ -33,6 +43,10 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
+  // check all fields avaialble or not
+  if (!req.body.email || !req.body.password)
+    throw new Error("email,password are required fields");
+
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user || !(await user.comparePassword(password))) {
