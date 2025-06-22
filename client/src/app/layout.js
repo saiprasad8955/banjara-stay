4c2dccbd36c7a009a2450c2e34d49602e62a3c0b
saiprasadrathod1 <saiprasad.rathod1@symbtechnologies.com>
2025-06-22 14:54:19 +0530

    fix:image added

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -43,7 +43,7 @@ export const metadata = {
     siteName: 'BanjaraStay',
     images: [
       {
-        url: '/logo/logo_single.png',
+        url: 'https://banjara-stay-w88q.vercel.app/logo/logo_single.png',
         width: 1200,
         height: 630,
         alt: 'BanjaraStay preview image',
